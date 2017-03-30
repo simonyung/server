a9f0ad49a3460016e1bd57e958d001599a19b519
simonyung <simon246809@gmail.com>
2017-03-29 19:17:31 -0700

    my fix migrate problem

--- db/migrate/20170220143212_create_comments.rb
+++ db/migrate/20170220143212_create_comments.rb
@@ -1,5 +1,6 @@
 class CreateComments < ActiveRecord::Migration[5.0]
   def change
+    drop_table :comments
     create_table :comments do |t|
       t.text :comment
       t.references :post, foreign_key: true
--- db/schema.rb
+++ db/schema.rb
@@ -10,21 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170301155043) do
-
-  create_table "active_admin_comments", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
-    t.string   "namespace"
-    t.text     "body",          limit: 65535
-    t.string   "resource_id",                 null: false
-    t.string   "resource_type",               null: false
-    t.string   "author_type"
-    t.integer  "author_id"
-    t.datetime "created_at",                  null: false
-    t.datetime "updated_at",                  null: false
-    t.index ["author_type", "author_id"], name: "index_active_admin_comments_on_author_type_and_author_id", using: :btree
-    t.index ["namespace"], name: "index_active_admin_comments_on_namespace", using: :btree
-    t.index ["resource_type", "resource_id"], name: "index_active_admin_comments_on_resource_type_and_resource_id", using: :btree
-  end
+ActiveRecord::Schema.define(version: 20170330020502) do
 
   create_table "admin_users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "email",                  default: "", null: false
@@ -90,6 +76,7 @@ ActiveRecord::Schema.define(version: 20170301155043) do
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
+    t.string   "name"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
     t.string   "username"
